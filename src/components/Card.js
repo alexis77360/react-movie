3c252f61beeaf6582881379c1a63ad0a4795ad11
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-21 15:54:12 +0200

    ajout tri

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,6 +8,80 @@ const Card = ({movie}) => {
         let [yy,mm,dd] = date.split('-')
         //? on retourne le tableau avec les valeurs inversées
         return [dd,mm,yy].join('/')}
+
+
+        const genreFinder = (genreId) => {
+            let genreArray = [];
+            for (let i= 0; i < movie.genre_ids.length; i++) {
+                switch(movie.genre_ids[i]) {
+                    case 28:
+                        genreArray.push('Action')
+                        break;
+                    case 12:
+                        genreArray.push('Aventure')
+                        break;
+                    case 16:
+                        genreArray.push('Animation')
+                        break;
+                    case 35:
+                        genreArray.push('Comédie')
+                        break;
+                    case 80:
+                        genreArray.push('Crime')
+                        break;
+                    case 99:
+                        genreArray.push('Documentaire')
+                        break;
+                    case 18:
+                        genreArray.push('Drame')
+                        break;
+                    case 10751:
+                        genreArray.push('Familial')
+                        break;
+                    case 14:
+                        genreArray.push('Fantastique')
+                        break;
+                    case 36:    
+                        genreArray.push('Histoire')
+                        break;
+                    case 27:    
+                        genreArray.push('Horreur')
+                        break;  
+                    case 10402:
+                        genreArray.push('Musique')
+                        break;
+                    case 9648:
+                        genreArray.push('Mystère')
+                        break;
+                    case 10749:
+                        genreArray.push('Romance')  
+                        break;
+                    case 878:
+                        genreArray.push('Science-Fiction')
+                        break;
+                    case 10770:
+                        genreArray.push('Téléfilm')
+                        break;
+                    case 53:
+                        genreArray.push('Thriller')
+                        break;
+                    case 10752:
+                        genreArray.push('Guerre')
+                        break;
+                    case 37:
+                        genreArray.push('Western')
+                        break;
+                    default:
+                        break;
+                         
+                
+            }
+        }
+        //? on retourne le tableau avec les valeurs du nouveau tableau de genre
+        return genreArray.map((genre) => <li key={genre}>{genre}</li>)
+    }
+
+
     
     
 
@@ -22,7 +96,16 @@ const Card = ({movie}) => {
             : "./img/poster.jpg"} alt={movie.title} />
             <h2>{movie.title}</h2>
             {movie.release_date ? (<h5>Sorti le : {dateFormater(movie.release_date)}</h5> ) : null}
-            <h4>Note : {movie.vote_average}/10 ⭐</h4>
+            <h4>Note : {movie.vote_average}/10 <span>⭐</span></h4>
+            <ul>
+                {
+                    movie.genre_ids ? genreFinder() : null
+                }
+            </ul>
+{/*             //? si il y a une description on affiche le titre sinon on affiche rien
+ */}            {movie.overview ? (<h3>Synopsis</h3>) : ""}
+            <p>{movie.overview}</p>
+            <div className="btn">Ajouter au coups de coeur</div>
 
 
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -7,14 +7,24 @@ import Card from './Card';
 
 const Form = () => {
 
+    //? on crée un state pour stocker les données de l'api
     const[moviesData, setMoviesData] = useState([])
 
+    //? on crée un state pour stocker la valeur de l'input
+    const [search, setSearch] = useState('code')
 
+    //? on crée un state pour stocker la valeur du tri
+    const [sortGoodBad, setSortGoodBad] = useState(null)
+
+
+    //? on crée un useEffect pour faire la requête à l'api avec une requête get pour récupérer les données
     useEffect(() => {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR')
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${search}&language=fr-FR`)
         .then((res) => setMoviesData(res.data.results))
 
-    }, [])
+    },
+    //? on met search dans les dépendances pour que le useEffect se lance à chaque fois que search change
+     [search])
 
 
 
@@ -22,17 +32,39 @@ const Form = () => {
         <div className="form-component">
             <div className="form-container">
                 <form>
-                    <input type="text" placeholder="Entrer le titre d'un film" id='search-input' />
+                    <input type="text" placeholder="Entrer le titre d'un film" id='search-input' 
+                    onChange={(e) => setSearch(e.target.value)} />
+
                     <input type="submit" value="Rechercher" id='submit-btn' />
                 </form>
 
                 <div className="btn-sort-container">
-                    <div className="btn-sort" id="goodToBad">Top 🤩</div>
-                    <div className="btn-sort" id="badToGood"> Flop 🤮 </div>
+                    <div className="btn-sort" id="goodToBad"
+                    onClick={() => setSortGoodBad('goodToBad')}
+                    
+                    >Top 🤩</div>
+                    <div className="btn-sort" id="badToGood"
+                    onClick={() => setSortGoodBad('badToGood')}
+                    
+                    > Flop 🤮 </div>
                 </div>
 
                 <div className="result">
-                    {moviesData.slice(0, 12).map((movie) =>
+                    {moviesData
+                    
+                    .slice(0, 12)
+                    .sort((a, b) => {
+                        if (sortGoodBad === 'goodToBad') {
+                            return b.vote_average - a.vote_average
+                        } else if (sortGoodBad === 'badToGood') {
+                            return a.vote_average - b.vote_average
+                        } else {
+                            return null
+                        }
+                    })
+
+
+                    .map((movie) =>
                     
                     <Card movie={movie} key={movie.id} />
                     )}
